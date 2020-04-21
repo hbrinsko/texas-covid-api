2c1c283f73a38fec2b2a37c13721455793cb2941
Hannah Brinsko <h.brinsko@utexas.edu>
2020-04-20 22:34:57 -0500

    additional endpoints

--- api.py
+++ api.py
@@ -6,17 +6,19 @@ import json
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-class county:
-  def __init__(self, name,population,cases):
-    self.name = name
-    self.population = population
-    self.cases = cases
+def buildTimeline(headers, row):
+    col = 2
+    timeline = {}
+    while col < len(row):
+        timeline[headers[col]] = row[col]
+        col += 1
+    return timeline
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Texas Department of State Health Services CoVid-19 Data</h1><p>API for tracking the Covid Outbreak in Texas</p>"
 
-@app.route('/api/v1/counties/latest', methods=['GET'])
+@app.route('/api/v1/counties/latest/all', methods=['GET'])
 def counties_all():
     with open('counties.csv', 'r') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=',')
@@ -30,5 +32,64 @@ def counties_all():
                 }
                 counties.append(county)
     return jsonify(counties)
+
+@app.route('/api/v1/counties/latest', methods=['GET'])
+def counties_county():
+    if 'county' in request.args:
+        name = request.args['county']
+        name = name.lower()
+    else:
+        return "Error: No county provided. Please specify a county, ex: Travis."
+
+    with open('counties.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=',')
+            next(csv_reader)
+            for row in csv_reader:
+                if row[0].lower() == name:
+                    county = {
+                        'name': row[0],
+                        'population': row[1],
+                        'count': row[-1]
+                    }
+                    return jsonify(county)
+    return 'County not valid'
+
+@app.route('/api/v1/counties/timeline/all', methods=['GET'])
+def timelines_all():
+    with open('counties.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=',')
+            headers = next(csv_reader)
+            counties = []
+            for row in csv_reader:
+                county = {
+                    'name': row[0],
+                    'population': row[1],
+                    'count': row[-1],
+                    'timeline': buildTimeline(headers, row)
+                }
+                counties.append(county)
+    return jsonify(counties)
+
+@app.route('/api/v1/counties/timeline', methods=['GET'])
+def timeline_county():
+    if 'county' in request.args:
+        name = request.args['county']
+        name = name.lower()
+    else:
+        return "Error: No county provided. Please specify a county, ex: Travis."
+
+    with open('counties.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=',')
+            headers = next(csv_reader)
+            for row in csv_reader:
+                if row[0].lower() == name:
+                    county = {
+                        'name': row[0],
+                        'population': row[1],
+                        'count': row[-1],
+                        'timeline': buildTimeline(headers, row)
+                    }
+                    return jsonify(county)
+    return 'County not valid'
 
 app.run()
