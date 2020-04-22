b753e45df236f0cdf6a8e73a413561d18afa1e48
Hannah Brinsko <h.brinsko@utexas.edu>
2020-04-21 21:25:41 -0500

    daily change

--- api.py
+++ api.py
@@ -2,10 +2,21 @@ import flask
 from flask import request, jsonify
 import csv
 import json
-from collections import OrderedDict
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+def buildDailyChangeTimeline(headers, row):
+    col = 2
+    prev = 0
+    timeline = {}
+    while col < len(row):
+        new = int(row[col])
+        diff = new-prev
+        timeline[headers[col]] = diff
+        prev = new
+        col += 1
+    return timeline
 
 def buildTimeline(headers, row):
     col = 2
@@ -19,8 +30,8 @@ def buildTimeline(headers, row):
 def home():
     return "<h1>Texas Department of State Health Services CoVid-19 Data</h1><p>API for tracking the Covid Outbreak in Texas</p>"
 
-@app.route('/api/v1/counties/latest/all', methods=['GET'])
-def counties_all():
+@app.route('/api/v1/latest/all', methods=['GET'])
+def latest_all():
     with open('counties.csv', 'r') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=',')
             next(csv_reader)
@@ -34,8 +45,8 @@ def counties_all():
                 counties.append(county)
     return jsonify(counties)
 
-@app.route('/api/v1/counties/latest', methods=['GET'])
-def counties_county():
+@app.route('/api/v1/latest', methods=['GET'])
+def latest_county():
     if 'county' in request.args:
         name = request.args['county']
         name = name.lower()
@@ -55,7 +66,7 @@ def counties_county():
                     return jsonify(county)
     return 'County not valid'
 
-@app.route('/api/v1/counties/timeline/all', methods=['GET'])
+@app.route('/api/v1/timeline/all', methods=['GET'])
 def timelines_all():
     with open('counties.csv', 'r') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=',')
@@ -71,7 +82,7 @@ def timelines_all():
                 counties.append(county)
     return jsonify(counties)
 
-@app.route('/api/v1/counties/timeline', methods=['GET'])
+@app.route('/api/v1/timeline', methods=['GET'])
 def timeline_county():
     if 'county' in request.args:
         name = request.args['county']
@@ -89,6 +100,28 @@ def timeline_county():
                         'population': row[1],
                         'count': row[-1],
                         'timeline': buildTimeline(headers, row)
+                    }
+                    return jsonify(county)
+    return 'County not valid'
+
+@app.route('/api/v1/dailychange', methods=['GET'])
+def daily_change_county():
+    if 'county' in request.args:
+        name = request.args['county']
+        name = name.lower()
+    else:
+        return "Error: No county provided. Please specify a county, ex: Travis."
+
+    with open('counties.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=',')
+            headers = next(csv_reader)
+            for row in csv_reader:
+                if row[0].lower() == name:
+                    county = {
+                        'name': row[0],
+                        'population': row[1],
+                        'count': row[-1],
+                        'timeline': buildDailyChangeTimeline(headers, row)
                     }
                     return jsonify(county)
     return 'County not valid'
